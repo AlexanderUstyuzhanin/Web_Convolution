86d10b5f0e9ddb7c48660b44daedbb387357222a
vdib <vdib@users.noreply.github.com>
2017-05-22 00:05:26 +0200

    Add files via upload

---
+++ audioConv.js
@@ -0,0 +1,68 @@
+function audioConv(){
+  // Check for the various File API support.
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+  } else {
+    alert('The File APIs are not fully supported in this browser.');
+  }
+
+  var output;
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var convolver = audioCtx.createConvolver();
+
+  function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // // files is a FileList of File objects. List some properties.
+    // var output = [];
+    // for (var i = 0, f; f = files[i]; i++) {
+    //   output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+    //   f.size, ' bytes, last modified: ',
+    //   f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+    //   '</li>');
+    //
+    // }
+    // document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+
+
+    var inputSound = document.getElementById('inputSound');
+    inputSound.src = URL.createObjectURL(this.files[0]);
+
+    outputSound.src = inputSound.src;
+    //source.connect(audioCtx.destination);
+  }
+
+  function handleImResFileSelect(evt){
+
+    var impulseResponseSound = document.getElementById('impulseResponse');
+    impulseResponseSound.src = URL.createObjectURL(this.files[0]);
+    var files = evt.target.files;
+    //var response = audioCtx.createMediaElementSource(impulseResponseSound);
+    // var myAudio = document.querySelector('#inputSound2');
+
+    var imResponse = document.querySelector('#impulseResponse');
+    var fileReader = new FileReader;
+    fileReader.readAsArrayBuffer(this.files[0]);
+
+    fileReader.onload = function(){
+         var arrayBuffer = this.result;
+         audioCtx.decodeAudioData(arrayBuffer, function(buffer) {
+             convolver.buffer = buffer;
+           });
+         }
+
+
+  }
+
+  function playOutput(evt){
+    output.connect(convolver);
+    convolver.connect(audioCtx.destination);
+  }
+
+  document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  document.getElementById('iFiles').addEventListener('change', handleImResFileSelect, false);
+  document.getElementById('outputSound').addEventListener('play', playOutput, false);
+
+  var outputSound = document.getElementById('outputSound');
+  output = audioCtx.createMediaElementSource(outputSound);
+}
