0c15b6be02eb6ffde643dd687109d3a74e82b147
nicolagiaconi <nicola.giaconi@haw-hamburg.de>
2017-06-03 11:51:02 +0200

    Updated convolution.js
    This file now includes also cross-correlation.
    examples:
    conv([11,23,4,5,6,2],[9,5,7]);    //performs convolution
    xcross([11,23,4,5,6,2],[9,5,7]); //performs cross-correlation

--- js/convolution.js
+++ js/convolution.js
@@ -1,15 +1,31 @@
+//Author: Nicola Giaconi
+
+//ZERO_PADDING
 function zeroPad(x1, x2){
 	//Input: Two arrays, returns the zero padded arrays
 	x1=x1.concat(Array(x2.length-1).fill(0));
 	x2=x2.concat(Array(x1.length-1).fill(0));
 	return [x1,x2];
 }
+
+//FLIPPING ARRAY
+function flip(x){
+	//returns an array which is the index-reversed version of the input array
+	N=x.length;
+	x_flipped=Array(N).fill(0); 
+	for(i=0;i<N;++i){
+		x_flipped[N-i-1] = x[i];
+	}
+	return x_flipped;
+}
+
+//CONVOLUTION
 function conv(x1, x2){
 	//returns the convolution of the input arrays
-	x=zeroPad(x1,x2);
-	x1=x[0]; x2=x[1]; N=x[0].length;
-	y=Array(N).fill(0); //preallocating
-	for(n=0;n<N;++n){
+	x=zeroPad(x1,x2); 					//Zero-padding
+	x1=x[0]; x2=x[1]; N=x[0].length;	
+	y=Array(N).fill(0); 				//Preallocating
+	for(n=0;n<N;++n){					//for-loop implementation following definition
 		for(k=0;k<N;++k){
 			if(k>n) break;
 			y[n] += x1[k]*x2[n-k];
@@ -18,12 +34,17 @@ function conv(x1, x2){
 	return y;
 }
 
-//TO BE IMPLEMENTED
-function convFast(x1,x2){//need to find a working library for fft and ifft (with standard radix2 butterfly algorithm)
-	x=zeroPad(x1,x2);
-	x1=x[0]; x2=x[1];
-	return ifft(fft(x1)*fft(x2));
+//CORRELATION
+function xcross(x1, x2){
+	//returns the cross-correlation of the input arrays					
+	x=zeroPad(x1,flip(x2)); 			//Zero-padding and index-reversing
+	x1=x[0]; x2=x[1]; N=x[0].length;	
+	y=Array(N).fill(0); 				//Preallocating
+	for(n=0;n<N;++n){					//for-loop implementation following definition
+		for(k=0;k<N;++k){
+			if(k>n) break;
+			y[n] += x1[k]*x2[n-k];
+		}
+	}
+	return y;
 }
-
-console.log(conv([11,23,4,5,6,2],[9,5,7]));
-//console.log(convFast([11,23,4,5,6,2],[9,5,7]));
