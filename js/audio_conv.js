700d8f59fee4a013f87766ade67578cbb8d6bdc5
vdib <adib.ali@outlook.com>
2017-11-09 23:38:44 +0100

    removed unnecessary variables, beautified

--- js/audio_conv.js
+++ js/audio_conv.js
@@ -10,34 +10,24 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 
 
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var convolver = audioCtx.createConvolver();
 var offlineCtx;
 var offlineConvolver;
 var convolverBuffer;
 var offlineContextSource;
 var inputAudioBuffer;
-
-var audioPreset = document.getElementById('audioPreset');
-var irPresetList = document.getElementById('irPresetList');
-var inputSound = document.getElementById('inputSound');
-
-var fileArrayBufferDecoded;
-var IRFileArrayBufferDecoded;
 var ownAudioFileURL;
 var inputAudioPresetURL;
 var ownIRFileURL;
 var irPresetURL;
-var presetAudioBuffer;
-var buffer = presetAudioBuffer;
-var ownIRFileBuffer;
 var outputBlobURL;
 var blob;
-var worker;
-var files;
 
+var audioPreset = document.getElementById('audioPreset');
+var irPresetList = document.getElementById('irPresetList');
+var inputSound = document.getElementById('inputSound');
+var outputSound = document.getElementById('outputSound');
 
 function onAudioSelect(evt) {
-
     var selectedPreset = audioPreset.value;
 
     switch (selectedPreset) {
@@ -53,7 +43,6 @@ function onAudioSelect(evt) {
 
     //Sets the player's src to control playback
     inputSound.src = inputAudioPresetURL;
-    // outputSound.src = inputAudioPresetURL;
 
     //Check radio button
     document.getElementById('audioPresetRadio').checked = true;
@@ -70,14 +59,7 @@ function onAudioSelect(evt) {
     ajaxRequest.send();
 }
 
-function onAudioPresetClick() {
 
-}
-
-function onOwnAudioClick() {
-    setInputBuffer(fileArrayBufferDecoded);
-    inputSound.src = ownAudioFileURL;
-}
 
 function handleAudioFileSelect(evt) {
     var files = evt.target.files; // FileList object
@@ -90,12 +72,8 @@ function handleAudioFileSelect(evt) {
     fileReader.readAsArrayBuffer(this.files[0]);
     fileReader.onload = function () {
         var fileArrayBuffer = this.result;
-        audioCtx.decodeAudioData(fileArrayBuffer, function (buffer) {
-            fileArrayBufferDecoded = buffer;
-            setInputBuffer(buffer);
-        });
+        audioCtx.decodeAudioData(fileArrayBuffer, setInputBuffer);
     }
-    // outputSound.src = ownAudioFileURL;
     document.getElementById('ownAudioFile').checked = true;
 }
 
@@ -106,14 +84,6 @@ setInputBuffer = function (buffer) {
         inputAudioBuffer.copyToChannel(buffer.getChannelData(channel), channel);
     }
 
-}
-
-setConvolverBuffer = function (buffer) {
-
-    convolverBuffer = audioCtx.createBuffer(buffer.numberOfChannels, buffer.length, buffer.sampleRate);
-    for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
-        convolverBuffer.copyToChannel(buffer.getChannelData(channel), channel);
-    }
 }
 
 function loadIRPreset(audioURL) {
@@ -129,7 +99,6 @@ function loadIRPreset(audioURL) {
     }
     ajaxRequest.send();
 }
-
 
 function onIrPresetSelect(evt) {
     var selectedPreset = irPresetList.value;
@@ -169,6 +138,7 @@ function handleIRFileSelect(evt) {
 
     var impulseResponseSound = document.getElementById('impulseResponse');
     ownIRFileURL = URL.createObjectURL(this.files[0]);
+    //Set src of the player
     impulseResponseSound.src = ownIRFileURL;
     var files = evt.target.files;
 
@@ -176,69 +146,63 @@ function handleIRFileSelect(evt) {
     fileReader.readAsArrayBuffer(this.files[0]);
     fileReader.onload = function () {
         var IRFileArrayBuffer = this.result;
-        audioCtx.decodeAudioData(IRFileArrayBuffer, function (buffer) {
-            IRFileArrayBufferDecoded = buffer;
-            setConvolverBuffer(buffer);
-        });
+        audioCtx.decodeAudioData(IRFileArrayBuffer, setConvolverBuffer);
     }
 
 
     document.getElementById('ownIrFile').checked = true;
 }
 
-function onIRPresetClick() {
-    loadIRPreset(irPresetURL);
-    convolverBuffer = presetAudioBuffer;
-}
-
-function onOwnIRClick() {
-    loadIRPreset(ownIRFileURL);
-    convolverBuffer = ownIRFileBuffer;
-}
+setConvolverBuffer = function (buffer) {
+    
+        convolverBuffer = audioCtx.createBuffer(buffer.numberOfChannels, buffer.length, buffer.sampleRate);
+        for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
+            convolverBuffer.copyToChannel(buffer.getChannelData(channel), channel);
+        }
+    }
 
 function convolve() {
-
-    offlineCtx = new OfflineAudioContext(2, 48000 * 45, 48000);
+    //Offline context can only be used once. New one has to be recreated everytime
+    //Convolution result length is the sum of both input lengths
+    offlineCtx = new OfflineAudioContext(2, inputAudioBuffer.length + convolverBuffer.length, inputAudioBuffer.sampleRate);
     offlineContextSource = offlineCtx.createBufferSource();
     offlineContextSource.buffer = inputAudioBuffer;
-
+    
     offlineConvolver = offlineCtx.createConvolver();
     offlineConvolver.buffer = convolverBuffer;
-
+    
     offlineContextSource.connect(offlineConvolver);
     offlineConvolver.connect(offlineCtx.destination);
     offlineContextSource.start();
-
+    
     offlineCtx.startRendering().then(function (renderedBuffer) {
         console.log("rendering complete");
-
+        
         // start a new worker 
-        worker = new Worker('js/libs/recorderWorker.js');
-
+        var worker = new Worker('js/libs/recorderWorker.js');
+        
         // initialize the new worker
         worker.postMessage({
             command: 'init',
             config: { sampleRate: offlineCtx.sampleRate }
         });
-
+        
         // callback for `exportWAV`
         worker.onmessage = function (e) {
             blob = e.data;
             outputBlobURL = URL.createObjectURL(blob);
             outputSound.src = outputBlobURL;
         };
-
-        var shortBuffer = trimBuffer(renderedBuffer);
-
+        
         // send the channel data from our buffer to the worker
         worker.postMessage({
             command: 'record',
             buffer: [
-                shortBuffer.getChannelData(0),
-                shortBuffer.getChannelData(1)
+                renderedBuffer.getChannelData(0),
+                renderedBuffer.getChannelData(1)
             ]
         });
-
+        
         // ask the worker for a WAV
         worker.postMessage({
             command: 'exportWAV',
@@ -247,39 +211,33 @@ function convolve() {
     });
 }
 
-//Removes trailing zeroes in audio buffer because buffer is fixed size and convolution result is shorter
-function trimBuffer(audioBuffer) {
-    channel0Data = audioBuffer.getChannelData(0);
-    channel1Data = audioBuffer.getChannelData(1);
-    let frameNumber = audioBuffer.length;
-    while (channel0Data[frameNumber - 1] == 0) {
-        frameNumber--;
-    }
-    channel0Data = channel0Data.slice(0, frameNumber);
-    channel1Data = channel1Data.slice(0, frameNumber);
-    var newBuffer = audioCtx.createBuffer(2, frameNumber, 48000);
-    newBuffer.copyToChannel(channel0Data, 0);
-    newBuffer.copyToChannel(channel1Data, 1);
-
-    return newBuffer;
-}
-
 function downloadFile() {
-
-
     var filename = "output.wav";
     var element = document.createElement('a');
-
+    
     element.setAttribute('href', outputBlobURL);
     element.setAttribute('download', filename);
-
+    
     element.style.display = 'none';
     document.body.appendChild(element);
-
+    
     element.click();
-
+    
     document.body.removeChild(element);
+    
+}
 
+function onOwnAudioClick() {
+    setInputBuffer(fileArrayBufferDecoded);
+    inputSound.src = ownAudioFileURL;
+}
+
+function onIRPresetClick() {
+    loadIRPreset(irPresetURL);
+}
+
+function onOwnIRClick() {
+    loadIRPreset(ownIRFileURL);
 }
 
 //Event listeners 
@@ -293,8 +251,6 @@ document.getElementById('audioPresetRadio').addEventListener('click', onAudioSel
 document.getElementById('ownAudioFile').addEventListener('click', onOwnAudioClick, false);
 document.getElementById('irPresetRadio').addEventListener('click', onIRPresetClick, false);
 document.getElementById('ownIrFile').addEventListener('click', onOwnIRClick, false);
+
 document.getElementById('downloadButton').addEventListener("click", downloadFile, false);
-
-var outputSound = document.getElementById('outputSound');
-
 document.getElementById('convolve').addEventListener('click', convolve, false);
